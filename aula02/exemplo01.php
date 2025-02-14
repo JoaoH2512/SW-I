7750208efc79c518f6b9f2c2b53a69a20a982880
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2025-02-14 16:55:03 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+    <p>Oi sou o João</p>
+    <?php echo "<p>Oi sou o João</p>";
+    $nome = "João";
+    echo "<p>Oi sou o $nome</p>";
+    echo "<p>Oi sou o".$nome."</p>";
+    ?>
+</body>
+</html>
