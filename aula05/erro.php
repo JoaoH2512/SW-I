3bcdb442177fb3f7eab1e3af53d7a093fd43382d
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2025-03-28 14:57:36 -0300

    Add files via upload

---
+++ aula05/erro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de Erro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo_erro.css">
+</head>
+<body>
+ <div class="container" >
+        <div class="error-page d-flex align-items-center justify-content-center">
+            <div class="container text-center">
+                <h1 class="error-code glitch mb-4">404</h1>
+                <h2 class="display-6 mb-3">32)(*@#</h2>
+                <p class="lead mb-5">Oops! Looks like this page got scrambled</p>
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="index.php" class="btn btn-primary">Go Home</a>                    
+                </div>
+        </div>
+   </div>
+
+ </div>
+</body>
+</html>
