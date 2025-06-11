01bbd52af1d6d50e0ceb158ca835076d00e03874
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2025-06-11 16:40:00 -0300

    Add files via upload

---
+++ aula13/exercicio02/Livro.class.php
@@ -0,0 +1,16 @@
+<?php 
+
+    class Livro{
+
+        private $titulo;
+        private $autor;
+        private $disponivel;
+
+        public function __construct($t,$a,$d){
+
+            
+        }
+    }
+
+
+?>
